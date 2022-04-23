169435f8c7ff1c23b1d70d5008890d01f1494241
Italo Yoshito <italofuji@gmail.com>
2022-04-23 15:37:45 -0300

    nvim configuration files

---
+++ init.lua
@@ -0,0 +1,11 @@
+require 'user.options'
+require 'user.keymaps'
+require 'user.plugins'
+require 'user.colorscheme'
+require 'user.nvim-tree'
+require 'user.completions'
+require "user.toggle-terminal"
+require "user.telescope"
+require "user.auto-pairs"
+require "user.bufferline"
+
