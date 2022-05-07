d232561ea03b4d197cecf88f002e3f25d9d53b56
Italo Yoshito <italofuji@gmail.com>
2022-05-07 15:27:47 -0300

    chore: add icon for dockerfiles for nvim-web-devicons plugin

--- lua/user/web-dev-icons.lua
+++ lua/user/web-dev-icons.lua
@@ -3,6 +3,11 @@ require("nvim-web-devicons").set_icon {
     icon = "",
     color = "#e8e8e8",
     name = "txt"
+  },
+  Dockerfile = {
+    icon = "",
+    color = "#45baf5",
+    name = "Dockerfile"
   }
 }
 
