e990fc001e598db35b6f57fc9f41940af701f3b1
Italo Yoshito <italofuji@gmail.com>
2022-05-17 20:20:34 -0300

    chore: enable auto format code on save

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -43,4 +43,5 @@ end
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+-- auto formats on save
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
